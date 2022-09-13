f5ef0cc860679e6be3a68395ed114f906659edec
Akash <karmakarakash659@gmail.com>
2022-09-13 20:44:15 +0530

    'prepared'

---
+++ backend/data/products.js
@@ -0,0 +1,164 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 3,
+    numReviews: 5,
+  },
+  {
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 6,
+  },
+  {
+    name: 'Sony A6000  DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Sony A6000 further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 10,
+    rating: 3.5,
+    numReviews: 70,
+  },
+  {
+    name: 'Sony Playstation 5 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 4.6,
+    numReviews: 60,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 2.5,
+    numReviews: 5,
+  },{
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 8,
+    rating: 3.5,
+    numReviews: 9,
+  },{
+    name: 'Nike-Golden edition-4',
+    image: '/images/gold-nike.jpg',
+    description:
+      'Originally created for the hardwood, the Dunk later took to the streets—and as they say, the rest is history. More than 35 years after its debut, the silhouette still delivers bold, defiant style and remains a coveted look for crews across both sport and culture.',
+    brand: 'Nike',
+    category: 'Sports',
+    price: 129.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 6,
+  },{
+    name: 'Kookaburaa Mongoose Bat',
+    image: '/images/bat.jpg',
+    description:
+      'A mongoose cricket bat has a short blade and a long handle. A mongoose bat is very different from a conventional bat. It varies in its shape from a normal bat. ',
+    brand: 'Realme',
+    category: 'sports',
+    price: 200.99,
+    countInStock: 4,
+    rating: 5,
+    numReviews: 1,
+  },{
+    name: 'Tshirt Barcelona',
+    image: '/images/barca-tshirt.jpg',
+    description:
+      'Barca designs, manufactures, distributes and sells fitness, running and crossfit sportswear including clothing and footwear. The clothing line includes t-shirts, hoodies, jackets and pants among other items. Polyester mesh fabric tee with vector branding',
+    brand: 'Apple',
+    category: 'Clothes',
+    price: 399.99,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 6,
+  },{
+    name: 'DJI- Maverick-99',
+    image: '/images/drone.jpg',
+    description:
+      'DJI Mavic 3 is a powerful flagship camera drone equipped with a 4/3 CMOS Hasselblad camera to facilitate professional-level imaging.',
+    brand: 'DJI',
+    category: 'Electronics',
+    price: 490.99,
+    countInStock: 7,
+    rating: 4.8,
+    numReviews: 7,
+  },{
+    name: 'Apple Watch Series 4 GPS + Cellular - 44 mm Gold Stainless Steel Case with Stone Sport Band',
+    image: '/images/watch.jpg',
+    description:
+      'You are looking at the Apple Watch Series 4 +Cellular. This unisex smart watch by Apple is light in weight and sits comfortably on your wrist ',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 245.99,
+    countInStock: 5,
+    rating: 5,
+    numReviews: 7,
+  },{
+    name: 'Cycle Bmw  7gear',
+    image: '/images/Bmw-cycle.jpg',
+    description:
+      'Dynamics, style and functionality – that’s what the newly designed and constructed BMW Bikes stand for. Inspired by the fixie trend, they now take the stage with completely new and innovative frame design ',
+    brand: 'BMW',
+    category: 'motor-bike',
+    price: 1345.99,
+    countInStock: 5,
+    rating: 3.5,
+    numReviews: 5,
+  },{
+    name: 'ULTRA 4k LED display TV',
+    image: '/images/Xiaomi-Mi-TV.jpg',
+    description:
+      'Unmatched picture quality delivered on the Mi TV 4X 50. The Mi TV 4X 50 has a 4K HDR 10-bit display that ensures a truly entertaining viewing experience ',
+    brand: 'Xiaomi',
+    category: 'Electronics',
+    price: 545.99,
+    countInStock: 5,
+    rating: 4,
+    numReviews: 5,
+  },
+  {
+    name: 'Iphone 13',
+    image: '/images/Apple-iPhone-13.jpg',
+    description:
+      'Our most advanced dual‐camera system ever. An even brighter Oled display. A lightning‐fast chip that leaves the competition behind. A huge leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 645.99,
+    countInStock: 5,
+    rating: 2.5,
+    numReviews: 10,
+  }]
+
+
+export default products
