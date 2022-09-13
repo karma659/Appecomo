06d32bf35a5a4416b4bec35ad79190489647fe82
Akash <karmakarakash659@gmail.com>
2022-09-13 21:15:59 +0530

    'updated'

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Ecomoweb',
+  description: 'We sell  amzing products',
+  keywords: 'electronics,sports, everything',
 }
 
 export default Meta
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -35,7 +35,7 @@ const LoginScreen = ({ location, history }) => {
       {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId='email'>
+        <Form.Group className=" p-2 m-2 " controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type='email'
@@ -45,7 +45,7 @@ const LoginScreen = ({ location, history }) => {
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='password'>
+        <Form.Group className=" p-2 m-2 " controlId='password'>
           <Form.Label>Password</Form.Label>
           <Form.Control
             type='password'
@@ -55,7 +55,7 @@ const LoginScreen = ({ location, history }) => {
           ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button className=" p-2 m-2 " type='submit' variant='primary'>
           Sign In
         </Button>
       </Form>
