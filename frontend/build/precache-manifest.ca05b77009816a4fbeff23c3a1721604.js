b5f7a85540d4b623b4da46a84d30e16349580eb4
Akash <karmakarakash659@gmail.com>
2022-11-05 01:39:49 +0530

    first commit

---
+++ frontend/build/precache-manifest.ca05b77009816a4fbeff23c3a1721604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795fe98a37d823232f42df9eb155ae8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e200c9670c1f82ff239e",
+    "url": "/static/css/main.bca9092f.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e200c9670c1f82ff239e",
+    "url": "/static/js/main.2e719c9b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
